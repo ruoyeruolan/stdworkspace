f0d093215e87743b9a2a56feb68981116ab4844f
ryrl9703 <18762739367@163.com>
2025-01-20 14:12:41 +0800

    sync

--- target/debug/deps/adder-1e3d6994d313e56e.d
+++ target/debug/deps/adder-1e3d6994d313e56e.d
@@ -1,5 +1,5 @@
-/Users/wakala/IdeaProjects/Projects/rust/stdworkspace/target/debug/deps/adder-1e3d6994d313e56e: adder/src/lib.rs
+/Users/wakala/IdeaProjects/Projects/rust/rustdocment/target/debug/deps/adder-1e3d6994d313e56e: adder/src/lib.rs
 
-/Users/wakala/IdeaProjects/Projects/rust/stdworkspace/target/debug/deps/adder-1e3d6994d313e56e.d: adder/src/lib.rs
+/Users/wakala/IdeaProjects/Projects/rust/rustdocment/target/debug/deps/adder-1e3d6994d313e56e.d: adder/src/lib.rs
 
 adder/src/lib.rs:
--- target/debug/deps/adder-4be0c160b1bc8994.d
+++ target/debug/deps/adder-4be0c160b1bc8994.d
@@ -1,5 +1,5 @@
-/Users/wakala/IdeaProjects/Projects/rust/stdworkspace/target/debug/deps/libadder-4be0c160b1bc8994.rmeta: adder/src/lib.rs
+/Users/wakala/IdeaProjects/Projects/rust/rustdocment/target/debug/deps/libadder-4be0c160b1bc8994.rmeta: adder/src/lib.rs
 
-/Users/wakala/IdeaProjects/Projects/rust/stdworkspace/target/debug/deps/adder-4be0c160b1bc8994.d: adder/src/lib.rs
+/Users/wakala/IdeaProjects/Projects/rust/rustdocment/target/debug/deps/adder-4be0c160b1bc8994.d: adder/src/lib.rs
 
 adder/src/lib.rs:
