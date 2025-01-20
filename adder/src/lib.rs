a88f0e373994327931d09ce1a117202c247680eb
ryrl9703 <18762739367@163.com>
2025-01-20 14:05:07 +0800

    sync

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,9 +1,31 @@
+// @Introduce  : 
+// @File       : lib.rs
+// @Author     : ryrl
+// @Email      : ryrl970311@gmail.com
+// @Time       : 2025/01/19 22:07
+// @Description:
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
 #[cfg(test)]
 mod tests {
+    use core::panic;
+
     use super::*;
 
     #[test]
@@ -11,4 +33,19 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+    // #[test]
+    // fn another() {
+    //     panic!("Make this test fail");
+    // }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle { width: 8, height: 7 };
+        let smaller = Rectangle { width: 5, height: 1 };
+
+        assert!(larger.can_hold(&smaller));
+    }
 }
+
+
