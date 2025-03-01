5d7f408950134e22f184321fa921eca3ce9025d9
ruoyeruolan <ryrl970311@GMAIL.COM>
2025-03-01 19:10:04 +0800

    sync

--- practices/src/bin/apply_operations.rs
+++ practices/src/bin/apply_operations.rs
@@ -5,8 +5,6 @@
 // @Time       : 2025/03/01 18:23
 // @Description:
 
-use core::num;
-
 use practices::Solution;
 
 pub trait ApplyOperations {
@@ -15,14 +13,18 @@ pub trait ApplyOperations {
 
 impl ApplyOperations for Solution {
     fn apply_operations(nums: Vec<i32>) -> Vec<i32> {
+        let mut nums = nums;
         let n = nums.len();
-        let res = Vec::with_capacity(n);
 
-        for idx in 0..n {
-            if nums[idx] == nums[idx + 1] {
-                // let nums[idx] = 2 * nums[idx];
+        for idx in 0..n - 1 {
+            if nums[idx] == nums[idx + 1] && nums[idx] != 0 {
+                nums[idx] *= 2;
+                nums[idx + 1] = 0;
             }
         }
+
+        let mut res: Vec<i32> = nums.into_iter().filter(|&x| x != 0).collect();
+        res.resize(n, 0);
         res
     }
 }
